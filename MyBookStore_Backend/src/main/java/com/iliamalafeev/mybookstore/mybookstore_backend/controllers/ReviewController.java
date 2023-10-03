88b4945a5241e792801f84099119669f5860e429
Ilia Malafeev <12qwerty98@gmail.com>
2023-10-03 20:16:00 +0200

    Cross origin added, paginated response added

--- MyBookStore_Backend/src/main/java/com/iliamalafeev/mybookstore/mybookstore_backend/controllers/ReviewController.java
+++ MyBookStore_Backend/src/main/java/com/iliamalafeev/mybookstore/mybookstore_backend/controllers/ReviewController.java
@@ -1,16 +1,17 @@
 package com.iliamalafeev.mybookstore.mybookstore_backend.controllers;
 
-import com.iliamalafeev.mybookstore.mybookstore_backend.entities.Review;
+import com.iliamalafeev.mybookstore.mybookstore_backend.dto.ReviewDTO;
 import com.iliamalafeev.mybookstore.mybookstore_backend.services.ReviewService;
 import com.iliamalafeev.mybookstore.mybookstore_backend.utils.error_responses.BookErrorResponse;
 import com.iliamalafeev.mybookstore.mybookstore_backend.utils.exceptions.BookException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
+@CrossOrigin("http://localhost:5173/")
 @RestController
 @RequestMapping("/api/reviews")
 public class ReviewController {
@@ -23,9 +24,11 @@ public class ReviewController {
     }
 
     @GetMapping("/{bookId}")
-    public List<Review> findAllByBookId(@PathVariable("bookId") Long bookId) {
+    public Page<ReviewDTO> findAllByBookId(@PathVariable("bookId") Long bookId,
+                                           @RequestParam(value = "page", required = true) Integer page,
+                                           @RequestParam(value = "reviews-per-page", required = true) Integer reviewsPerPage) {
 
-        return reviewService.findAllByBookId(bookId);
+        return reviewService.findAllByBookId(bookId, PageRequest.of(page, reviewsPerPage));
     }
 
     @ExceptionHandler
