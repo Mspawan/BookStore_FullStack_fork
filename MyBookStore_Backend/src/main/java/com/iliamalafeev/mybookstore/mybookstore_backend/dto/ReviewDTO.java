26b84a996829f884c28557232c453d68b9895345
Ilia Malafeev <12qwerty98@gmail.com>
2023-10-03 20:18:22 +0200

    Entity refactored

--- MyBookStore_Backend/src/main/java/com/iliamalafeev/mybookstore/mybookstore_backend/dto/ReviewDTO.java
+++ MyBookStore_Backend/src/main/java/com/iliamalafeev/mybookstore/mybookstore_backend/dto/ReviewDTO.java
@@ -4,9 +4,19 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 public class ReviewDTO {
+
+    private Long id;
+
+    private String personEmail;
+
+    private String personFirstName;
+
+    private LocalDateTime date;
 
     @NotNull(message = "Rating must be present")
     private Double rating;
