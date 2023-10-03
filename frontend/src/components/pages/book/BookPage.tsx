279555a5ea8110cb26e1d27d17f4d16d8f158064
Ilia Malafeev <12qwerty98@gmail.com>
2023-10-03 20:13:32 +0200

    Book page content added

--- frontend/src/components/pages/book/BookPage.tsx
+++ frontend/src/components/pages/book/BookPage.tsx
@@ -1,6 +1,43 @@
+import { useState } from "react";
 import { Quote } from "../../commons/quote/Quote"
+import { BookModel } from "../../../models/BookModel";
+import { useFetchBook } from "../../../utils/useFetchBook";
+import { BookPageBookCard } from "./components/BookPageBookCard";
+import { LoadingSpinner } from "../../commons/loading_spinner/LoadingSpinner";
+import { useAuthenticationContext } from "../../../authentication/authenticationContext";
+import { useFetchBookReviews } from "../../../utils/useFetchBookReviews";
+import { ReviewModel } from "../../../models/ReviewModel";
+import { useCheckIfBookReviewedByUser } from "../../../utils/useCheckIfBookReviewedByUser";
 
 export const BookPage = () => {
+
+    const bookId = (window.location.pathname).split('/')[2];
+
+    const { authentication } = useAuthenticationContext();    
+
+    // Book state
+    const [book, setBook] = useState<BookModel | null>(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState<string | null>(null);
+
+    // Book Reviews state
+    const [reviews, setReviews] = useState<ReviewModel[]>([]);
+    const [totalStars, setTotalStars] = useState(0);
+    const [isLoadingReviews, setIsLoadingReviews] = useState(true);
+    const [reviewsHttpError, setReviewsHttpError] = useState<string | null>(null);
+
+    // User review state
+    const [isReviewLeft, setIsReviewLeft] = useState(false);
+    const [isLoadingUserReview, setIsLoadingUserReview] = useState(true);
+    const [userReviewHttpError, setUserReviewHttpError] = useState<string | null>(null);
+    
+    const urlPaginationParams = `?page=0&reviews-per-page=5`;
+
+    useFetchBook(bookId, setBook, setIsLoading, setHttpError);
+
+    useFetchBookReviews(bookId, setReviews, setTotalStars, setIsLoadingReviews, setReviewsHttpError, urlPaginationParams, 0);
+
+    useCheckIfBookReviewedByUser(bookId, authentication.token, setIsReviewLeft, setIsLoadingUserReview, setUserReviewHttpError);
 
     return (
 
@@ -8,7 +45,27 @@ export const BookPage = () => {
 
             <Quote quoteId={6} />
 
-            Book Page
+            {!isLoading ? 
+
+                <>
+
+                    {!httpError ? 
+
+                        <div className="max-container px-5 py-10">
+
+                            <BookPageBookCard book={book} />
+
+                        </div>
+                        
+                        : <div className="max-container px-5 py-10">{httpError}</div>
+
+                    }
+
+                </>
+                
+                : <LoadingSpinner />
+            
+            }
 
         </section>
 
---
+++ frontend/src/components/pages/book/components/BookPageBookCard.tsx
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom"
+import { BookModel } from "../../../../models/BookModel"
+
+type BookCardProps = {
+    book: BookModel | null
+}
+
+export const BookPageBookCard = ({ book }: BookCardProps) => {
+
+    if (book === null) return <div>Something went wrong - cannot read the book data</div>
+
+    return (
+
+        <div className="flex max-lg:flex-col items-start max-lg:items-center gap-10 p-10 rounded-lg w-full shadow-custom relative" key={book.id}>
+
+            <img src={book.img} alt="cover" width={250} height={400} className="shadow-xl "/>
+            
+            <div className="flex flex-col gap-10 lg:w-1/3">
+
+                <div className="max-lg:text-center">
+                
+                    <p className="font-semibold lg:text-3xl max-lg:text-2xl">{book.title}</p>
+                    <p className="font-light lg:text-2xl max-lg:text-xl">{book.author}</p>
+
+                </div>
+                
+                <div className="flex gap-5 max-lg:justify-center">
+
+                    {book.genres.map(
+
+                        (genre) => <div key={genre.description} className="bg-teal-100 py-2 px-4 rounded-md">{genre.description}</div>
+
+                    )}
+
+                </div>
+
+                <div className="max-lg:text-center">
+
+                    {book.description} Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Quos nemo, sunt exercitationem consectetur sint expedita magni beatae placeat 
+                    dolorem assumenda libero nobis impedit rem modi in a illum qui. Porro ad voluptate 
+                    voluptas suscipit, libero facilis cumque? Nulla ut quia ad odio voluptatem autem 
+                    molestiae deleniti corporis voluptas ab distinctio quaerat explicabo repellendus 
+                    perferendis ullam fugit odit dolores porro obcaecati nostrum vero rerum, id laboriosam!
+
+                </div>
+            
+            </div>
+
+        </div>
+
+    )
+
+}
