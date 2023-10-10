3f46b3f4f198d391fada2e68cc7dd980d51dd084
Ilia Malafeev <12qwerty98@gmail.com>
2023-10-10 20:37:26 +0200

    Content added to shelf page

--- frontend/src/components/pages/shelf/ShelfPage.tsx
+++ frontend/src/components/pages/shelf/ShelfPage.tsx
@@ -1,6 +1,9 @@
 import { Navigate } from "react-router-dom";
 import { Quote } from "../../commons/quote/Quote"
 import { useAuthenticationContext } from "../../../authentication/authenticationContext";
+import { useState } from "react";
+import { HistoryTab } from "./components/HistoryTab";
+import { LoansTab } from "./components/LoansTab";
 
 export const ShelfPage = () => {
 
@@ -8,13 +11,37 @@ export const ShelfPage = () => {
 
     if (!authentication.isAuthenticated) return <Navigate to={"/"} />
 
+    const [isHistoryTabSelected, setIsHistoryTabSelected] = useState(false);
+
     return (
 
         <section className="mt-[70px] w-full flex flex-col items-center gap-10">
 
             <Quote quoteId={2} />
 
-            Shelf Page
+            <div className="max-container w-full p-5 flex max-lg:flex-col gap-14 relative">                
+
+                <div className="flex lg:flex-col gap-5">
+
+                    <button className={`${!isHistoryTabSelected && "bg-teal-100 shadow-custom lg:shadow-md"} nav-link`} onClick={() => setIsHistoryTabSelected(false)}>
+                        Checkouts
+                    </button>
+
+                    <button className={`${isHistoryTabSelected && "bg-teal-100 shadow-custom lg:shadow-md"} nav-link`} onClick={() => setIsHistoryTabSelected(true)}>
+                        History
+                    </button>
+
+                </div>
+
+                <div className="bg-teal-600 absolute lg:w-[1px] lg:top-0 lg:bottom-0 lg:left-36 max-lg:h-[1px] max-lg:left-5 max-lg:right-5 max-lg:top-24" />
+
+                <div className="w-full">
+
+                    { isHistoryTabSelected ? <HistoryTab /> : <LoansTab /> }
+
+                </div>
+
+            </div>
 
         </section>
 
