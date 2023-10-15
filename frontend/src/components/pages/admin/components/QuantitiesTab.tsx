f856ee698d3cecba433484b7c7cd258532a1f72a
Ilia Malafeev <12qwerty98@gmail.com>
2023-10-15 15:51:46 +0200

    Quantities tab content added

--- frontend/src/components/pages/admin/components/QuantitiesTab.tsx
+++ frontend/src/components/pages/admin/components/QuantitiesTab.tsx
@@ -1,10 +1,96 @@
+import { useState } from "react";
+import { BookModel } from "../../../../models/BookModel";
+import { useFetchBooks } from "../../../../utils/useFetchBooks";
+import { Pagination } from "../../../commons/pagination/Pagination";
+import { LoadingSpinner } from "../../../commons/loading_spinner/LoadingSpinner";
+import { QuantitiesTabBookCard } from "./QuantitiesTabBookCard";
+
 export const QuantitiesTab = () => {
+
+    const [books, setBooks] = useState<BookModel[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState<string | null>(null);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalAmountOfBooks, setTotalAmountOfBooks] = useState(0);
+    const [totalPages, setTotlalPages] = useState(0);
+    const [titleQuery, setTitleQuery] = useState("");
+    const [resultRange, setResultRange] = useState({start: 1, end: 5});
+    const [searchParams, setSearchParams] = useState("");
+
+    const handleSearchClick = () => {
+
+        setHttpError(null);
+        if (titleQuery !== "") setSearchParams(`/search/by-title?title-query=${titleQuery}`);
+    };
+
+    const urlPaginationParams = (searchParams === "" ? "?" : "&") + `page=${currentPage - 1}&books-per-page=5`;
+
+    useFetchBooks(urlPaginationParams, currentPage, setBooks, setIsLoading, setHttpError, setTotalAmountOfBooks, setTotlalPages, searchParams);
+
+    // console.log(totalAmountOfBooks);
 
     return (
 
-        <div>
-            
-            QuantitiesTab
+        <div className="flex flex-col gap-10">
+
+            <div className="flex gap-5">
+
+                <input className="input" placeholder="Search books by title..." value={titleQuery} onChange={event => setTitleQuery(event.target.value)} />
+
+                <button className="btn-main bg-teal-800 text-teal-100 hover:text-teal-800" onClick={() => handleSearchClick()}>
+                    Search
+                </button>
+
+            </div>
+
+            {isLoading ? <LoadingSpinner /> :
+
+                <>
+
+                    {httpError ? <div>{httpError}</div> :
+                        
+                        <>
+
+                            {totalAmountOfBooks > 0 ?
+                                
+                                <>
+
+                                    <div className="sm:text-xl flex gap-5 items-center justify-center">
+
+                                        Books: 
+
+                                        <p className="sm:text-3xl max-sm:text-xl text-teal-600">
+                                            {resultRange.start} - {totalAmountOfBooks <= 5 ? totalAmountOfBooks : resultRange.end}
+                                        </p> 
+
+                                        out of 
+
+                                        <p className="sm:text-3xl max-sm:text-xl text-teal-600">{totalAmountOfBooks}</p>
+
+                                    </div>
+                                
+                                    {books.map(
+                                        
+                                        book => <QuantitiesTabBookCard key={book.id} book={book} />
+                                        
+                                    )}
+
+                                </>
+
+                                : <div>Nothing was found</div>
+
+                            }
+
+                        </>
+
+                    }
+
+                </>
+
+            }
+
+            <Pagination currentPage={currentPage} totalPages={totalPages} totalAmountOfItems={totalAmountOfBooks} 
+                setCurrentPage={setCurrentPage} setResultRange={setResultRange} />
             
         </div>
 
---
+++ frontend/src/components/pages/admin/components/QuantitiesTabBookCard.tsx
@@ -0,0 +1,132 @@
+import { useState } from "react";
+import { BookModel } from "../../../../models/BookModel"
+import { FormLoader } from "../../../commons/form_loader/FormLoader";
+
+type BookCardProps = {
+    book: BookModel
+}
+
+export const QuantitiesTabBookCard = ({ book }: BookCardProps) => {
+
+    const [totalQuantity, setTotalQuantity] = useState(book.copies);
+    const [availableQuantity, setAvailableQuantity] = useState(book.copiesAvailable);
+
+    const [isLoadingChangeQuantity, setIsLoadingChangeQuantity] = useState(false);
+    const [isLoadingDeleteBook, setIsLoadingDeleteBook] = useState(false);
+    const [changeQuantityHttpError, setChangeQuantityHttpError] = useState<string | null>(null);
+    const [deleteBookHttpError, setDeleteBookHttpError] = useState<string | null>(null);
+
+    const handleChangeQuantityClick = (operation: string) => {
+
+        if (operation === "increase") console.log("increase clicked");
+        else if (operation === "decrease") console.log("decrease clicked");
+    }
+
+    const handleDeleteBookClick = () => {
+
+        console.log("delete clicked");
+    }
+
+    return (
+
+        <div className="flex max-lg:flex-col lg:items-start max-lg:items-center gap-10 p-5 rounded-md shadow-custom-2" key={book.id}>
+
+            <img src={book.img} alt="cover" width={200} height={320} className="shadow-xl max-lg:w-[200px]"/>
+            
+            <div className="flex flex-col gap-10 xl:w-5/12 lg:flex-1">
+
+                <div className="max-lg:text-center">
+                
+                    <p className="font-semibold lg:text-2xl max-lg:text-xl">{book.title}</p>
+                    <p className="font-light lg:text-xl max-lg:text-lg">{book.author}</p>
+
+                </div>
+
+                <div className="max-lg:text-center">
+
+                    {book.description} Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                    Quos nemo, sunt exercitationem consectetur sint expedita magni beatae placeat 
+                    dolorem assumenda libero nobis impedit rem modi in a illum qui. Porro ad voluptate 
+                    voluptas suscipit, libero facilis cumque? Nulla ut quia ad odio voluptatem autem 
+                    molestiae deleniti corporis voluptas ab distinctio quaerat explicabo repellendus 
+                    perferendis ullam fugit odit dolores porro obcaecati nostrum vero rerum, id laboriosam!
+
+                </div>
+            
+            </div>
+
+            <div className="flex-1 border-2 border-teal-800 rounded-md p-4 flex flex-col gap-3 items-center">
+
+                <p className="text-xl font-semibold">Manage book options</p>
+
+                <div className="h-[1px] w-full bg-teal-800" />
+
+                <div className="flex gap-2 items-center text-lg text-center">
+
+                    <p>Total copies:</p>
+                    
+                    <p className="text-teal-600 font-semibold"> {totalQuantity}</p>
+
+                </div>
+
+                <div className="flex gap-2 items-center text-lg text-center">
+
+                    <p>Copies Available:</p>
+                    
+                    <p className="text-teal-600 font-semibold"> {availableQuantity}</p>
+
+                </div>
+
+                <div className="flex flex-col gap-5">
+
+                    {(isLoadingChangeQuantity || isLoadingDeleteBook) ? <FormLoader isLoading={true} /> :
+
+                        <>
+
+                            {changeQuantityHttpError && 
+
+                                <div className="border border-red-500 py-1 px-2 bg-red-100 rounded-md text-center">
+                                    {changeQuantityHttpError}
+                                </div>
+
+                            }
+
+                            {deleteBookHttpError && 
+                            
+                                <div className="border border-red-500 py-1 px-2 bg-red-100 rounded-md text-center">
+                                    {deleteBookHttpError}
+                                </div>
+                                
+                            }
+
+                            <div className="flex gap-3 max-xl:flex-col">
+
+                                <button className="btn-main" onClick={() => handleChangeQuantityClick("increase")}>
+                                    Increase Quantity
+                                </button>
+
+                                <button className="btn-main" onClick={() => handleChangeQuantityClick("decrease")}>
+                                    Decrease Quantity
+                                </button>
+
+                            </div>
+
+                        </>
+
+                    }
+
+                </div>
+
+                <div className="h-[1px] w-full bg-teal-800" />
+
+                <button className="btn-main" onClick={handleDeleteBookClick}>
+                    Delete Book
+                </button>
+
+            </div>
+
+        </div>
+
+    )
+
+}
