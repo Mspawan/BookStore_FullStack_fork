8395bf37ab8b07c7c6651ff57cd6d2f3140edbf7
Ilia Malafeev <12qwerty98@gmail.com>
2023-10-15 12:22:23 +0200

    Genre controller and service added

---
+++ MyBookStore_Backend/src/main/java/com/iliamalafeev/mybookstore/mybookstore_backend/services/GenreService.java
@@ -0,0 +1,35 @@
+package com.iliamalafeev.mybookstore.mybookstore_backend.services;
+
+import com.iliamalafeev.mybookstore.mybookstore_backend.dto.GenreDTO;
+import com.iliamalafeev.mybookstore.mybookstore_backend.entities.Genre;
+import com.iliamalafeev.mybookstore.mybookstore_backend.repositories.GenreRepository;
+import jakarta.transaction.Transactional;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+public class GenreService {
+
+    private final ModelMapper modelMapper;
+    private final GenreRepository genreRepository;
+
+    @Autowired
+    public GenreService(ModelMapper modelMapper, GenreRepository genreRepository) {
+        this.modelMapper = modelMapper;
+        this.genreRepository = genreRepository;
+    }
+
+    public List<GenreDTO> findAll() {
+
+        return genreRepository.findAll().stream().map(this::convertToGenreDTO).collect(Collectors.toList());
+    }
+
+    private GenreDTO convertToGenreDTO(Genre genre) {
+        return modelMapper.map(genre, GenreDTO.class);
+    }
+}
