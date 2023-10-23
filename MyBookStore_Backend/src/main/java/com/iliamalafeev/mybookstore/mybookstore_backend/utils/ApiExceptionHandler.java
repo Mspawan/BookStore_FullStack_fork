395903ea9cbc08145c6c0ab110eed99bb5938d7e
Ilia Malafeev <12qwerty98@gmail.com>
2023-10-23 17:45:39 +0200

    Common exception handler added

---
+++ MyBookStore_Backend/src/main/java/com/iliamalafeev/mybookstore/mybookstore_backend/utils/ApiExceptionHandler.java
@@ -0,0 +1,49 @@
+package com.iliamalafeev.mybookstore.mybookstore_backend.utils;
+
+import com.iliamalafeev.mybookstore.mybookstore_backend.utils.error_responses.*;
+import com.iliamalafeev.mybookstore.mybookstore_backend.utils.exceptions.*;
+import io.jsonwebtoken.ExpiredJwtException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ApiExceptionHandler {
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    public ResponseEntity<ExpiredJwtErrorResponse> handleExpiredJwtException() {
+        ExpiredJwtErrorResponse response = new ExpiredJwtErrorResponse("Your authentication token is expired, please re-login.", System.currentTimeMillis());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(BookException.class)
+    private ResponseEntity<BookErrorResponse> handleException(BookException e) {
+        BookErrorResponse response = new BookErrorResponse(e.getMessage(), System.currentTimeMillis());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(DiscussionException.class)
+    private ResponseEntity<DiscussionErrorResponse> handleException(DiscussionException e) {
+        DiscussionErrorResponse response = new DiscussionErrorResponse(e.getMessage(), System.currentTimeMillis());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ReviewException.class)
+    private ResponseEntity<ReviewErrorResponse> handleException(ReviewException e) {
+        ReviewErrorResponse response = new ReviewErrorResponse(e.getMessage(), System.currentTimeMillis());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(PaymentException.class)
+    private ResponseEntity<PaymentErrorResponse> handleException(PaymentException e) {
+        PaymentErrorResponse response = new PaymentErrorResponse(e.getMessage(), System.currentTimeMillis());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(PersonException.class)
+    private ResponseEntity<PersonErrorResponse> handleException(PersonException e) {
+        PersonErrorResponse response = new PersonErrorResponse(e.getMessage(), System.currentTimeMillis());
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
