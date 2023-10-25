d4e6dfcbe69286e19b45e6120752fb6a7f55dc17
Ilia Malafeev <12qwerty98@gmail.com>
2023-10-25 23:42:24 +0200

    Payment controller added

---
+++ MyBookStore_Backend/src/main/java/com/iliamalafeev/mybookstore/mybookstore_backend/controllers/PaymentController.java
@@ -0,0 +1,54 @@
+package com.iliamalafeev.mybookstore.mybookstore_backend.controllers;
+
+import com.iliamalafeev.mybookstore.mybookstore_backend.dto.PaymentInfoDTO;
+import com.iliamalafeev.mybookstore.mybookstore_backend.entities.Payment;
+import com.iliamalafeev.mybookstore.mybookstore_backend.security.jwt.JwtUtils;
+import com.iliamalafeev.mybookstore.mybookstore_backend.services.PaymentService;
+import com.stripe.exception.StripeException;
+import com.stripe.model.PaymentIntent;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:5173/")
+@RestController
+@RequestMapping("/api/payment/secure")
+public class PaymentController {
+
+    private final PaymentService paymentService;
+    private final JwtUtils jwtUtils;
+
+    @Autowired
+    public PaymentController(PaymentService paymentService, JwtUtils jwtUtils) {
+        this.paymentService = paymentService;
+        this.jwtUtils = jwtUtils;
+    }
+
+    private String extractEmail(String token) {
+        String jwt = token.substring(7);
+        return jwtUtils.extractPersonEmail(jwt);
+    }
+
+    @GetMapping
+    public Double findByPersonEmail(@RequestHeader("Authorization") String token) {
+
+        return paymentService.findPaymentFeesByPersonEmail(extractEmail(token));
+    }
+
+    @PostMapping("/payment-intent")
+    public ResponseEntity<String> createPaymentIntent(@RequestBody PaymentInfoDTO paymentInfoDTO) throws StripeException {
+
+        PaymentIntent paymentIntent = paymentService.createPaymentIntent(paymentInfoDTO);
+        String paymentStr = paymentIntent.toJson();
+
+        return new ResponseEntity<>(paymentStr, HttpStatus.OK);
+    }
+
+    @PutMapping("/payment-complete")
+    public ResponseEntity<HttpStatus> stripePaymentComplete(@RequestHeader("Authorization") String token) {
+
+        paymentService.stripePayment(extractEmail(token));
+        return ResponseEntity.ok(HttpStatus.OK);
+    }
+}
