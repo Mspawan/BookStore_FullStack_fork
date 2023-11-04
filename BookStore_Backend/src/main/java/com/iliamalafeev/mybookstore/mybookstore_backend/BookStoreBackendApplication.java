745811167b45f1179671cb883ced8995a87c9238
Ilia Malafeev <12qwerty98@gmail.com>
2023-11-04 15:06:40 +0100

    Folder rename

--- BookStore_Backend/src/main/java/com/iliamalafeev/mybookstore/mybookstore_backend/BookStoreBackendApplication.java
+++ BookStore_Backend/src/main/java/com/iliamalafeev/mybookstore/mybookstore_backend/BookStoreBackendApplication.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class MyBookStoreBackendApplication {
+public class BookStoreBackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyBookStoreBackendApplication.class, args);
+        SpringApplication.run(BookStoreBackendApplication.class, args);
     }
 
     @Bean
