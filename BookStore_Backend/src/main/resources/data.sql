3f06f8bc9acd400a399ec3d3915b72462fd12eee
Ilia Malafeev <12qwerty98@gmail.com>
2023-11-09 18:55:44 +0100

    DataBase initialization files added.

---
+++ BookStore_Backend/src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into genre (id, description) values (1, 'Science Fiction');
+insert into genre (id, description) values (2, 'Fantasy');
+insert into genre (id, description) values (3, 'Adventure');
+insert into genre (id, description) values (4, 'Novel');
+insert into genre (id, description) values (5, 'Dystopia');
+insert into genre (id, description) values (6, 'Detective');
+insert into genre (id, description) values (7, 'Short Stories');
+insert into genre (id, description) values (8, 'Romance');
+insert into genre (id, description) values (9, 'LitRPG');
