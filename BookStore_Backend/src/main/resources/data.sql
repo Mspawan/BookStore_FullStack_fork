b912dc7cd4692360905307c3f07d1e69aa5e387c
Ilia Malafeev <12qwerty98@gmail.com>
2023-11-13 14:10:44 +0100

    SQL commands refactored.

--- BookStore_Backend/src/main/resources/data.sql
+++ BookStore_Backend/src/main/resources/data.sql
@@ -1,9 +1,9 @@
-insert into genre (id, description) values (1, 'Science Fiction');
-insert into genre (id, description) values (2, 'Fantasy');
-insert into genre (id, description) values (3, 'Adventure');
-insert into genre (id, description) values (4, 'Novel');
-insert into genre (id, description) values (5, 'Dystopia');
-insert into genre (id, description) values (6, 'Detective');
-insert into genre (id, description) values (7, 'Short Stories');
-insert into genre (id, description) values (8, 'Romance');
-insert into genre (id, description) values (9, 'LitRPG');
+insert into genre (id, description) values (1, 'Science Fiction') on conflict do nothing;
+insert into genre (id, description) values (2, 'Fantasy') on conflict do nothing;
+insert into genre (id, description) values (3, 'Adventure') on conflict do nothing;
+insert into genre (id, description) values (4, 'Novel') on conflict do nothing;
+insert into genre (id, description) values (5, 'Dystopia') on conflict do nothing;
+insert into genre (id, description) values (6, 'Detective') on conflict do nothing;
+insert into genre (id, description) values (7, 'Short Stories') on conflict do nothing;
+insert into genre (id, description) values (8, 'Romance') on conflict do nothing;
+insert into genre (id, description) values (9, 'LitRPG') on conflict do nothing;
