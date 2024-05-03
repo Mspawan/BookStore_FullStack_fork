1492fddfb56e0a938cb11bf05128ee683a4830a5
Ilia Malafeev <12qwerty98@gmail.com>
2024-05-03 19:56:51 +0200

    Code refactored.

--- BookStore_Backend/src/main/java/com/iliamalafeev/bookstore/bookstore_backend/dto/CheckoutDTO.java
+++ BookStore_Backend/src/main/java/com/iliamalafeev/bookstore/bookstore_backend/dto/CheckoutDTO.java
@@ -1,14 +1,10 @@
 package com.iliamalafeev.bookstore.bookstore_backend.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class CheckoutDTO {
 
     private BookDTO bookDTO;
