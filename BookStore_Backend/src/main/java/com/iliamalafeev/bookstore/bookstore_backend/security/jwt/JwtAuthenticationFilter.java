7b2349a0605dd733f9e19e0b7fbd4b0014bc4683
Ilia Malafeev <12qwerty98@gmail.com>
2024-05-03 20:17:38 +0200

    Code slightly refactored.

--- BookStore_Backend/src/main/java/com/iliamalafeev/bookstore/bookstore_backend/security/jwt/JwtAuthenticationFilter.java
+++ BookStore_Backend/src/main/java/com/iliamalafeev/bookstore/bookstore_backend/security/jwt/JwtAuthenticationFilter.java
@@ -33,7 +33,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(@NonNull HttpServletRequest request, @NonNull HttpServletResponse response,
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
                                     @NonNull FilterChain filterChain) throws ServletException, IOException {
 
         final String authHeader = request.getHeader("Authorization");
