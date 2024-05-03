d0e5cecf48e4450eef3d878d383a767f2bd6ae83
Ilia Malafeev <12qwerty98@gmail.com>
2024-05-03 18:55:41 +0200

    No args constructor added.

--- BookStore_Backend/src/main/java/com/iliamalafeev/bookstore/bookstore_backend/security/dto/requests/PersonRegistrationDTO.java
+++ BookStore_Backend/src/main/java/com/iliamalafeev/bookstore/bookstore_backend/security/dto/requests/PersonRegistrationDTO.java
@@ -11,7 +11,6 @@ import java.time.LocalDate;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class PersonRegistrationDTO {
 
     @NotBlank(message = "First name must contain at least 1 character")
