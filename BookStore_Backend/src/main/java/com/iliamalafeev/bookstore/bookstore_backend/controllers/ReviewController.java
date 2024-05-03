c51abcddc8c31814909affb26bd7fb66ab313412
Ilia Malafeev <12qwerty98@gmail.com>
2024-05-03 18:04:48 +0200

    Code slightly refactored, return types changed to ResponseEntity for consistency.

--- BookStore_Backend/src/main/java/com/iliamalafeev/bookstore/bookstore_backend/controllers/ReviewController.java
+++ BookStore_Backend/src/main/java/com/iliamalafeev/bookstore/bookstore_backend/controllers/ReviewController.java
@@ -7,6 +7,8 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("http://localhost:5173/")
@@ -22,19 +24,24 @@ public class ReviewController {
         this.reviewService = reviewService;
     }
 
-    @Operation(summary = "Get a paginated list of reviews for a specific book.", description = "Returns a page containing ReviewDTO objects.")
+    @Operation(summary = "Get a paginated list of reviews for a specific book.",
+            description = "Returns a page containing ReviewDTO objects.")
     @GetMapping("/{bookId}")
-    public Page<ReviewDTO> findAllByBookId(@PathVariable("bookId") Long bookId, @RequestParam(value = "page") Integer page,
-                                           @RequestParam(value = "reviews-per-page") Integer reviewsPerPage,
-                                           @RequestParam(value = "latest", defaultValue = "false") boolean latest) {
+    public ResponseEntity<Page<ReviewDTO>> findAllByBookId(@PathVariable("bookId") Long bookId,
+                                                           @RequestParam(value = "page") Integer page,
+                                                           @RequestParam(value = "reviews-per-page") Integer reviewsPerPage,
+                                                           @RequestParam(value = "latest", defaultValue = "false") boolean latest) {
 
-        return reviewService.findAllByBookId(bookId, PageRequest.of(page, reviewsPerPage), latest);
+        Page<ReviewDTO> responseBody = reviewService.findAllByBookId(bookId, PageRequest.of(page, reviewsPerPage), latest);
+        return new ResponseEntity<>(responseBody, HttpStatus.OK);
     }
 
-    @Operation(summary = "Get an average rating for a specific book.", description = "Counts an average rating across all the reviews related to selected book. Returns a value of type Double.")
+    @Operation(summary = "Get an average rating for a specific book.",
+            description = "Counts an average rating across all the reviews related to selected book. Returns a value of type Double.")
     @GetMapping("/average-rating/{bookId}")
-    public Double getAverageRatingByBookId(@PathVariable("bookId") Long bookId) {
+    public ResponseEntity<Double> getAverageRatingByBookId(@PathVariable("bookId") Long bookId) {
 
-        return reviewService.getAverageRatingByBookId(bookId);
+        Double responseBody = reviewService.getAverageRatingByBookId(bookId);
+        return new ResponseEntity<>(responseBody, HttpStatus.OK);
     }
 }
