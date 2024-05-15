8e2109b08f6944e13f847e51cd04a6028f92ab81
Ilia Malafeev <12qwerty98@gmail.com>
2024-05-15 17:47:36 +0200

    Code slightly refactored.

--- BookStore_Backend/src/test/resources/data.sql
+++ BookStore_Backend/src/test/resources/data.sql
@@ -1,12 +1,12 @@
--- Inserting 1 default ROLE_ADMIN user for easy testing, password = adminpassword --
+-- Inserting 1 default ROLE_ADMIN user for testing, password = adminpassword --
 insert into person (id, first_name, last_name, date_of_birth, email, password, role, registered_at) values (10000001, 'Admin', 'Admin', '2000-01-01', 'admin@email.com', '$2a$10$rW7AN0KxM4scb/65mKRLsulYZcMNOn3HynGSjSPjLA.9D5eQuQYk.', 'ROLE_ADMIN', '2023-11-23 13:23:05.262') on conflict do nothing;
 
--- Inserting 3 default ROLE_USER users for easy testing, password = userPassword --
+-- Inserting 3 default ROLE_USER users for testing, password = userPassword --
 insert into person (id, first_name, last_name, date_of_birth, email, password, role, registered_at) values (10000002, 'First Name 1', 'Last Name 1', '2000-01-01', 'email1@email.com', '$2a$10$sVk6EYpNZa8WELP4WbLT.OfLtR.WOuZkMP3eP1g.EOFbCZbQ5ZeeG', 'ROLE_USER', '2023-11-23 13:23:05.262') on conflict do nothing;
 insert into person (id, first_name, last_name, date_of_birth, email, password, role, registered_at) values (10000003, 'First Name 2', 'Last Name 2', '2000-01-01', 'email2@email.com', '$2a$10$sVk6EYpNZa8WELP4WbLT.OfLtR.WOuZkMP3eP1g.EOFbCZbQ5ZeeG', 'ROLE_USER', '2023-11-23 13:23:05.262') on conflict do nothing;
 insert into person (id, first_name, last_name, date_of_birth, email, password, role, registered_at) values (10000004, 'First Name 3', 'Last Name 3', '2000-01-01', 'email3@email.com', '$2a$10$sVk6EYpNZa8WELP4WbLT.OfLtR.WOuZkMP3eP1g.EOFbCZbQ5ZeeG', 'ROLE_USER', '2023-11-23 13:23:05.262') on conflict do nothing;
 
--- Inserting a few genres to initialize the default book stock --
+-- Inserting a few genres to initialize the default test book stock --
 insert into genre (id, description) values (1, 'Genre 1') on conflict do nothing;
 insert into genre (id, description) values (2, 'Genre 2') on conflict do nothing;
 
@@ -31,17 +31,17 @@ insert into book_genre (book_id, genre_id) values (10000005, 1) on conflict do n
 insert into review (id, person_email, person_first_name, person_last_name, book_id, date, rating, review_description) values (10000001, 'email1@email.com', 'First Name 1', 'Last Name 1', 10000001, '2024-01-01', 4.5, 'Description 1') on conflict do nothing;
 insert into review (id, person_email, person_first_name, person_last_name, book_id, date, rating, review_description) values (10000002, 'email2@email.com', 'First Name 2', 'Last Name 2', 10000001, '2024-01-01', 3.5, 'Description 2') on conflict do nothing;
 
--- Inserting payment fee for the first user for testing --
+-- Inserting test payment fee for the first user --
 insert into payment (id, person_email, amount) values (10000001, 'email1@email.com', 10.00) on conflict do nothing;
 
--- Inserting history record for the first user for testing --
+-- Inserting test history record for the first user --
 insert into history_record (id, person_email, book_id, checkout_date, return_date) values (10000001, 'email1@email.com', 10000001, current_date - 10, current_date -5) on conflict do nothing;
 
--- Inserting discussions for the first user for testing --
+-- Inserting test discussions for the first user --
 insert into discussion (id, person_email, title, question, admin_email, response, closed) values (10000001, 'email1@email.com', 'Title 1', 'Question 1', null, null, false) on conflict do nothing;
 insert into discussion (id, person_email, title, question, admin_email, response, closed) values (10000002, 'email1@email.com', 'Title 2', 'Question 2', 'admin@email.com', 'Response 1', true) on conflict do nothing;
 
--- Inserting checkouts for the users for testing --
+-- Inserting test checkouts for different users --
 insert into checkout (id, person_email, book_id, checkout_date, return_date) values (10000001, 'email1@email.com', 10000002, current_date, current_date + 7) on conflict do nothing;
 insert into checkout (id, person_email, book_id, checkout_date, return_date) values (10000002, 'email1@email.com', 10000003, current_date, current_date + 7) on conflict do nothing;
 insert into checkout (id, person_email, book_id, checkout_date, return_date) values (10000003, 'email2@email.com', 10000004, current_date - 3, current_date + 4) on conflict do nothing;
